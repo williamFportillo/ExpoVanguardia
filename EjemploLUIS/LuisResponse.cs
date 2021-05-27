33a531ab747af707cda6c076cfb48425432bd464
williamFportillo <portillowilliam18@gmail.com>
2021-05-26 22:57:44 -0600

    Subiendo expo de Vanguardia

---
+++ EjemploLUIS/LuisResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjemploLUIS
+{
+    public class LuisResponse
+    {
+        public Prediction Prediction { get; set; }
+    }
+
+    public class Prediction
+    {
+        public string TopIntent { get; set; }
+
+        public Entity Entities { get; set; }
+    }
+
+    public class Entity
+    {
+
+        public List<string> Name { get; set; }
+        public List<Age> Age { get; set; }
+    }
+
+    public class Age
+    {
+        public int Number { get; set; }
+
+        public string Units { get; set; }
+    }
+}
---
+++ EjemploLUIS/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace EjemploLUIS
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var httpclient = new HttpClient();
+            var query = "hola me llamo william y tengo 23 años";
+            var result = await httpclient.GetStringAsync("https://proyectovanguardia.cognitiveservices.azure.com/luis/prediction/v3.0/apps/bad92a72-8f96-4db8-b457-cded762e6a21/slots/production/predict?subscription-key=be1b4a87a109432daf5434da9e38ee89&verbose=true&show-all-intents=true&log=true&query="+query);
+            var luisResponse = JsonConvert.DeserializeObject<LuisResponse>(result);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
